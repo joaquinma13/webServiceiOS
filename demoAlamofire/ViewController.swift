bdca3b0e70f4b2d7121066b0ac2938592237913a
joaquinma1993 <joaquin.martinez@mccollect.com.mx>
2019-04-11 12:04:03 -0500

    Initial Commit

---
+++ demoAlamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demoAlamofire
+//
+//  Created by Joaquin Martinez on 11/04/19.
+//  Copyright © 2019 MC Collect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
